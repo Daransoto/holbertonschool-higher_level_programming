cc797d2790b4372c60ea4d51898fc90ad21469c2
Daransoto <901@holbertonschool.com>
2019-09-10 22:46:43 +0000

    Moved code to a function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    print("{}".format("FIzzBuzz" if not i % 3 and not i % 5 else "Fizz" if not
-                      i % 3 else "Buzz" if not i % 5 else i), end=" ")
-print()
+def fizzbuzz():
+    for i in range(1, 101):
+        print("{}".format("FIzzBuzz" if not i % 3 and not i % 5 else "Fizz"
+                          if not i % 3 else "Buzz" if not i % 5 else i),
+              end=" ")
+    print()
