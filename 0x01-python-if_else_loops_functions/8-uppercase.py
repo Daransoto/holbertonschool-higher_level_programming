ff3af98bf362eb4d4fd679e03d0627db255e3a28
Daransoto <901@holbertonschool.com>
2019-09-10 22:03:56 +0000

    Fixed edge cases

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-            print("{}".format(str[i] if ord(str[i]) <= 97 or ord(str[i]) >=
+            print("{}".format(str[i] if ord(str[i]) < 97 or ord(str[i]) >
                               123 else chr(ord(str[i]) - 32)), end="")
     print()
