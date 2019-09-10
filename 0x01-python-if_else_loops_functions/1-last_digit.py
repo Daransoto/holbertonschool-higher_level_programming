19fb7886f5d7f2735d7e7893d585488fc7787801
Daransoto <901@holbertonschool.com>
2019-09-10 20:11:06 +0000

    Changed the way of putting the sign on negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number / abs(number) * (abs(number) % 10)
+last = abs(number) % 10
+if number < 0:
+    last *= -1
 if last > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, int(last)))
