5880cf17e8964442750c48d71a4b0b129ec23aec
Daransoto <901@holbertonschool.com>
2019-09-10 22:51:18 +0000

    Removed line jump at the end

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,4 +4,3 @@ def fizzbuzz():
         print("{}".format("FIzzBuzz" if not i % 3 and not i % 5 else "Fizz"
                           if not i % 3 else "Buzz" if not i % 5 else i),
               end=" ")
-    print()
