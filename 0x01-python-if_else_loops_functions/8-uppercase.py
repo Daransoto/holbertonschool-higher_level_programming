50692951902300b80dbaac31128a71302e2f70a6
Daransoto <901@holbertonschool.com>
2019-09-10 21:55:17 +0000

    Fixed case when letter is already uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 123:
             print("{}".format(str[i] if ord(str[i]) <= 97 or
                     ord(str[i]) >= 123 else chr(ord(str[i]) - 32)), end="")
     print()
