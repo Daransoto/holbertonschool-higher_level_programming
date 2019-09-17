5391db8f2e178d72585a56820dc73847dd151bf6
Daransoto <901@holbertonschool.com>
2019-09-17 19:35:12 +0000

    Reversing the original string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None
-        return None
-    for i in range(len(my_list) - 1, -1, -1):
+    my_list.reverse()
+    for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
