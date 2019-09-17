e8cd7bb04b41ce9c4a4ae786f6ad3e61ad59ca7b
Daransoto <901@holbertonschool.com>
2019-09-17 19:42:05 +0000

    Fixed condition of printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    if my_list is None:
-        return
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    if my_list is not None:
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
