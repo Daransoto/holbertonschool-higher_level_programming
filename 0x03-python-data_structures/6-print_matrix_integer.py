168526a91d10173b02c2ed580f4b32fdc787d55f
Daransoto <901@holbertonschool.com>
2019-09-17 20:12:33 +0000

    Removed casting to string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-            print('{}'.format(' ').join(map(str, row)))
+        count = 0
+        for elem in row:
+            count += 1
+            print('{}'.format(elem), end=(" " if count < len(row) else ""))
+        print()
