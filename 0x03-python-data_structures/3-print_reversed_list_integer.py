6412ca1e39379ba9b7976ee01e8fd008768a72b6
Daransoto <901@holbertonschool.com>
2019-09-17 19:37:06 +0000

    Printing new line if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
+    if my_list is None
+        print()
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
