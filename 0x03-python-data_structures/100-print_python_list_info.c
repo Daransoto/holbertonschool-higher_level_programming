09aed7514f4787a5e8bfb1bc5c32ed0da14fb50e
Daransoto <901@holbertonschool.com>
2019-09-18 18:47:00 +0000

    Removed extra space

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,7 +10,7 @@ void print_python_list_info(PyObject *p)
 	int i;
 
 	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(p));
-	printf("[*] Allocated =  %d\n", (int)(((PyListObject *)p)->allocated));
+	printf("[*] Allocated = %d\n", (int)(((PyListObject *)p)->allocated));
 	for (i = 0; i < (int)PyList_Size(p); i++)
 		printf("Element %d: %s\n", i, PyList_GetItem(p, i)->ob_type->tp_name);
 
