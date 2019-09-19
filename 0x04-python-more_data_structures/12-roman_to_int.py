557371f6bb3c71e97f5fcb1736a28d1a0339b541
Daransoto <901@holbertonschool.com>
2019-09-19 18:14:20 +0000

    Added lowercase letters

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,9 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return
-    conv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    conv = {"I": 1, "i": 1, "V": 5, "v": 5, "X": 10, "x": 10, "L": 50,
+            "l": 50, "C": 100, "c": 100, "D": 500, "d": 500, "M": 1000,
+            "m": 1000}
     length = len(roman_string) - 1
     res = conv[roman_string[length]]
     for iterator in range(length, 0, -1):
