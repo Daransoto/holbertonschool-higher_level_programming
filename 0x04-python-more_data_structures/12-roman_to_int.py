00b5a439a4f2420555177a19f85670524994368e
Daransoto <901@holbertonschool.com>
2019-09-19 18:20:00 +0000

    Added special case when different letters in roman string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,11 +6,15 @@ def roman_to_int(roman_string):
             "l": 50, "C": 100, "c": 100, "D": 500, "d": 500, "M": 1000,
             "m": 1000}
     length = len(roman_string) - 1
+    if roman_string[length] not in conv:
+        return 0
     res = conv[roman_string[length]]
     for iterator in range(length, 0, -1):
         if roman_string[iterator] not in conv:
             return 0
         curr = conv[roman_string[iterator]]
+        if roman_string[iterator - 1] not in conv:
+            return 0
         prev = conv[roman_string[iterator - 1]]
         res += prev if prev >= curr else -prev
     return res
