07bd56a2de533019f5dc0d1bc6220a90e7d454b6
Daransoto <901@holbertonschool.com>
2019-09-19 18:41:20 +0000

    Added case when type passed to the function is not string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
     if not roman_string:
         return 0
     conv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
