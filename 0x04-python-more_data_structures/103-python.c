6d69ffb872ac4fa8e1903cc84d49622833c6abd7
Daransoto <901@holbertonschool.com>
2019-09-19 22:55:58 +0000

    Added call to print bytes when element of list is bytes

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <Python.h>
-#include <string.h>
+void print_python_bytes(PyObject *p);
 /**
 * print_python_list - Prints info about Python lists.
 * @p: Python object.
@@ -13,7 +13,11 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(p));
 	printf("[*] Allocated = %d\n", (int)(((PyListObject *)p)->allocated));
 	for (i = 0; i < (int)PyList_Size(p); i++)
+	{
 		printf("Element %d: %s\n", i, PyList_GET_ITEM(p, i)->ob_type->tp_name);
+		if (!strcmp(PyList_GET_ITEM(p, i)->ob_type->tp_name, "bytes"))
+			print_python_bytes(PyList_GET_ITEM(p, i));
+	}
 
 }
 /**
@@ -31,17 +35,17 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 	printf("  size: %ld\n", PyBytes_Size(p));
-    if (PyBytes_Size(p) > 0)
-    {
-	    printf("  trying string: %s\n", PyBytes_AsString(p));
-	    bytes_to_show = PyBytes_Size(p) + 1;
-	    if (bytes_to_show > 10)
-		    bytes_to_show = 10;
-    	printf("  first %d bytes:", bytes_to_show);
-	    for (i = 0; i < bytes_to_show; i++)
-    	{
-	    	printf(" %02x", (unsigned char)*(PyBytes_AsString(p) + i));
-    	}
-	    puts("");
-    }
+	if (PyBytes_Size(p) > 0)
+	{
+		printf("  trying string: %s\n", PyBytes_AsString(p));
+		bytes_to_show = PyBytes_Size(p) + 1;
+		if (bytes_to_show > 10)
+			bytes_to_show = 10;
+		printf("  first %d bytes:", bytes_to_show);
+		for (i = 0; i < bytes_to_show; i++)
+		{
+			printf(" %02x", (unsigned char)*(PyBytes_AsString(p) + i));
+		}
+		puts("");
+	}
 }
