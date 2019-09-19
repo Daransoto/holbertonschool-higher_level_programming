1b59e58078f5ca995019769529b7f867a82b33d6
Daransoto <901@holbertonschool.com>
2019-09-19 17:20:40 +0000

    Function that updates a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None:
+        a_dictionary = {key: value}
+        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
