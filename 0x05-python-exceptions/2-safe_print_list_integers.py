1fdf068eca87f9f63a5eb7d3d8e22796d689b619
Daransoto <901@holbertonschool.com>
2019-09-30 17:26:43 +0000

    Moved the count inside the try

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
                 print("{:d}".format(my_list[i]), end="")
+                count += 1
         except:
                 continue
-        count += 1
-    if count:
-        print()
+    print()
     return count
