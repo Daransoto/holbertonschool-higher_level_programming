ae155f3447b26cffb545735c747f7d4a6c50b536
Daransoto <901@holbertonschool.com>
2019-09-30 16:27:16 +0000

    Functions that safely print a list of integers and the result of a division using try - except

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+                print("{:d}".format(int(my_list[i])), end="")
+        except:
+                continue
+        count += 1
+    print()
+    return count
