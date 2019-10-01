07a1fb5c993f81fdb3bd1c285ecfab87bcad33a2
Daransoto <901@holbertonschool.com>
2019-10-01 22:02:24 +0000

    Fixed checking of int type on elements of tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,7 +41,7 @@ class Square:
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2 or\
-           value[0] is not int or value[1] is not int or value[0] < 0 or\
-           value[1] < 0:
+           type(value[0]) is not int or type(value[1]) is not int or\
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
