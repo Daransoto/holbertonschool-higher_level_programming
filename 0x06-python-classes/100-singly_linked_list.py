b6ab5a9461714a48878da84ff1aea72940dc8c5a
Daransoto <901@holbertonschool.com>
2019-10-02 01:11:09 +0000

    Function that creates a singly linked list and node classes

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        if type(data) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = data
+        if type(next_node) is not Node and next_node is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) is not Node and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+        elif self.__head.data >= value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            temp = self.__head
+            new.next_node = temp.next_node
+            while temp.next_node and value > temp.next_node.data:
+                new.next_node = temp.next_node.next_node
+                temp = temp.next_node
+            temp.next_node = new
+
+    def __str__(self):
+        res = ""
+        temp = self.__head
+        while temp is not None:
+            res = res + str(temp.data) + "\n"
+            temp = temp.next_node
+        res = res[:-1]
+        return res
