2f3a2d092600510e6062cb7f3c6734c8c7f7afe9
Daransoto <901@holbertonschool.com>
2019-10-05 05:26:29 +0000

    Removed blank line after documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,7 +20,6 @@ def add_integer(a, b=98):
         int: The addition of a and b.
 
     """
-
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
