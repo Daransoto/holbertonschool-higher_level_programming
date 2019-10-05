73eeba7c0c37abbf1f955b6cedc59b64c4a9c330
Daransoto <901@holbertonschool.com>
2019-10-05 06:27:26 +0000

    Testing with biggest number

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,4 +24,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    if a + 1 == a or b + 1 == b:
+        raise OverflowError("Number too large")
     return int(a) + int(b)
