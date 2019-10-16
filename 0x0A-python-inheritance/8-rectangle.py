85c94742813eb810fe36a87e660389a963c37541
Daransoto <901@holbertonschool.com>
2019-10-16 15:01:20 +0000

    Changed way to import parent class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python3
 """
-This module contains the classes:
-
--BaseGeometry.
--Rectangle.
+This module contains the class Rectangle.
 """
 
 
-class BaseGeometry:
-    """
-    Class that defines basic geometries.
-    """
-    def area(self):
-        """
-        Area of a geometry, not implemented by default.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Function that validates that value is integer.
-        """
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
-
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """
     Class that defines a rectangle.
