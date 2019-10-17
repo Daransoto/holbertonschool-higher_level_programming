05cf613548e73259494687dea21725488869957c
Daransoto <901@holbertonschool.com>
2019-10-17 02:17:18 +0000

    Fixed handling of index exception

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -23,7 +23,7 @@ while True:
                     print("{}: {:d}".format("File size", value))
                 elif value > 0:
                     print("{}: {:d}".format(key, value))
-    except (KeyboardInterrupt, EOFError):
+    except (KeyboardInterrupt, EOFError, IndexError):
         for key, value in sorted(res.items()):
             if key == "0":
                 print("{}: {:d}".format("File size", value))
