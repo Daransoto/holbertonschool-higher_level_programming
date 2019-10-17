e6bbe4af72a34c4eecfd2b34b55b091345ad4904
Daransoto <901@holbertonschool.com>
2019-10-17 00:43:29 +0000

    Solution to avanced task 2

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script parses logs.
+"""
+res = {"0": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+       "405": 0, "500": 0}
+i = 0
+while True:
+    try:
+        i += 1
+        logs = input()
+        res[logs.split(' ')[7]] += 1
+        res["0"] += int(logs.split(' ')[8])
+        if i % 10 == 0:
+            for key, value in sorted(res.items()):
+                if key == "0":
+                    print("{}: {:d}".format("File size", value))
+                elif value > 0:
+                    print("{}: {:d}".format(key, value))
+    except KeyboardInterrupt:
+        for key, value in sorted(res.items()):
+            if key == "0":
+                print("{}: {:d}".format("File size", value))
+            elif value > 0:
+                print("{}: {:d}".format(key, value))
+        break
