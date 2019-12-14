0716db2ea26237756f950f26303518c7c93d94c3
Daransoto <901@holbertonschool.com>
2019-12-14 20:21:02 +0000

    Added LIKE BINARY to ensure case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;"
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id;"
                 .format(sys.argv[4]))
     result = cur.fetchall()
     for row in result:
