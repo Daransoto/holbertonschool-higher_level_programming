ad5b66ae276927247f76e6cbabba1ea6f20970d0
Daransoto <901@holbertonschool.com>
2019-12-14 20:12:32 +0000

    Testing with python's startwith method

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name\
-                RLIKE '^N' ORDER BY id;")
+    cur.execute("SELECT * FROM states ORDER BY id;")
     result = cur.fetchall()
     for row in result:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cur.close()
     db.close()
