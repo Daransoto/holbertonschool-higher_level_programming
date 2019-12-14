e0c3195dbcefb54f49b0fa9e0636721974c8fbe7
Daransoto <901@holbertonschool.com>
2019-12-14 19:59:04 +0000

    First upload of task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# This script Lists all states from the database hbtn_0e_0_usa.
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM hbtn_0e_0_usa.states;")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    db.close()
