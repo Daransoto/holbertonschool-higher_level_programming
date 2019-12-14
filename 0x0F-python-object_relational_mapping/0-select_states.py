16928b0b1e250e4aa01871ca40b5a4e9a9541ea7
Daransoto <901@holbertonschool.com>
2019-12-14 20:01:05 +0000

    Added ORDER BY statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM hbtn_0e_0_usa.states;")
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
     result = cur.fetchall()
     for row in result:
         print(row)
