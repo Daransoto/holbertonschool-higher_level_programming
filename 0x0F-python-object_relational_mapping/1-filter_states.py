aaf8df0a467e1c8d8a84a20c7065821664860c6b
Daransoto <901@holbertonschool.com>
2019-12-14 20:09:28 +0000

    Changed LIKE for RLIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name\
-                LIKE 'N%' ORDER BY states.id;")
+                RLIKE '^N' ORDER BY id;")
     result = cur.fetchall()
     for row in result:
         print(row)
