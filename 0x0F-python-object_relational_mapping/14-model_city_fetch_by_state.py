fa34306580d300ef8f25d3eacdd8979846c6fad6
Daransoto <901@holbertonschool.com>
2019-12-15 03:27:23 +0000

    First upload of solution to task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Selecting all states and cities. """
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state, city in session.query(State, City).\
+            filter(State.id == City.state_id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
