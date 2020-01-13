da51d415488f08b93b4e5a50c2f47ef3b6377405
Daransoto <901@holbertonschool.com>
2020-01-13 20:28:26 +0000

    First upload of solution to task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Takes your Github credentials (username and password) and uses the Github
+    API to display your id. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get("https://api.github.com/user",
+                            auth=(sys.argv[1], sys.argv[2]))
+    if "id" in response.json():
+        print(response.json().get("id"))
+    else:
+        print("None")
