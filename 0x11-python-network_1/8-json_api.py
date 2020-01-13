9c45f967fdb9a5906a1ce3ac641f955ca510deda
Daransoto <901@holbertonschool.com>
2020-01-13 19:57:50 +0000

    Added prevention of execution when imported

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,18 @@ import requests
 import sys
 
 
-value = {"q": ""}
-if len(sys.argv) > 1:
-    value["q"] = sys.argv[1]
-response = requests.post("http://0.0.0.0:5000/search_user", data=value)
-try:
-    empty = ["", {}, None]
-    if "id" not in response.json().keys() or \
-       "name" not in response.json().keys():
-        print("No result")
-    else:
-        print("[{}] {}".format(response.json().get("id"),
-                               response.json().get("name")))
-except ValueError:
-    print("Not a valid JSON")
+if __name__ == "__main__":
+    value = {"q": ""}
+    if len(sys.argv) > 1:
+        value["q"] = sys.argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", data=value)
+    try:
+        empty = ["", {}, None]
+        if "id" not in response.json().keys() or \
+           "name" not in response.json().keys():
+            print("No result")
+        else:
+            print("[{}] {}".format(response.json().get("id"),
+                                   response.json().get("name")))
+    except ValueError:
+        print("Not a valid JSON")
