97f407f81d968778a7ea3702cb9929ad40423ca7
Daransoto <901@holbertonschool.com>
2020-01-13 19:33:49 +0000

    Using the correct ip

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,9 @@ import sys
 value = {"q": ""}
 if len(sys.argv) > 1:
     value["q"] = sys.argv[1]
-response = requests.post("http://0b77dee8aa88.19.hbtn-cod.io:5000/search_user",
-                         data=value)
+response = requests.post("http://0.0.0.0:5000/search_user", data=value)
 try:
-    if response.json() in ["", {}, None]:
+    if not response or response.json() in ["", {}, None]:
         print("No result")
     else:
         print("[{}] {}".format(response.json()["id"], response.json()["name"]))
