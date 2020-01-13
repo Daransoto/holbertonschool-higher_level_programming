a7f996da169524b1f0ee56fa4cecf04d10df88e3
Daransoto <901@holbertonschool.com>
2020-01-13 19:21:37 +0000

    First upload of solution to task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter. """
+import requests
+import sys
+
+
+value = {"q": ""}
+if len(sys.argv) > 1:
+    value["q"] = sys.argv[1]
+response = requests.post("http://0b77dee8aa88.19.hbtn-cod.io:5000/search_user",
+                         data=value)
+try:
+    if not response.json():
+        print("No result")
+    else:
+        print("[{}] {}".format(response.json()["id"], response.json()["name"]))
+except ValueError:
+    print("Not a valid JSON")
