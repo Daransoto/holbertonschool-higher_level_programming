ce07cdafd208c30bb436bdeb1d0170a95b1f0959
Daransoto <901@holbertonschool.com>
2020-02-04 20:55:53 +0000

    Added check for number of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const array = process.argv.slice(2).sort(function (a, b) { return b - a; });
-console.log(array[1]);
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2).sort(function (a, b) { return b - a; });
+  console.log(array[1]);
+}
