343d0c922ebc4399ec7049dfc3800cc31e1f67e7
Daransoto <901@holbertonschool.com>
2020-02-10 22:03:20 +0000

    First upload of advanced task 3

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+var fs = require('fs');
+let f1;
+let f2;
+let res;
+if (process.argv.length > 4) {
+  f1 = fs.readFileSync(process.argv[2], 'utf8');
+  f2 = fs.readFileSync(process.argv[3], 'utf8');
+  res = f1 + f2;
+  fs.writeFile(process.argv[4], res, function (err) { if (err) throw err; });
+}
