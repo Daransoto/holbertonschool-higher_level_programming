5f7730a697c9c9396c26ad4aa2ded0fe6cb5b858
Daransoto <901@holbertonschool.com>
2020-02-11 20:13:57 +0000

    Using filter instead of indexof

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,11 @@ request(process.argv[2], function (err, res, body) {
   else {
     const movies = JSON.parse(body).results;
     let count = 0;
+    let characters = [];
     for (const movie of movies) {
-      if (movie.characters.indexOf('https://swapi.co/api/people/18/') >= 0) count++;
+      characters = characters.concat(movie.characters);
     }
+    count = characters.filter(x => x.includes('18')).length;
     console.log(count);
   }
 });
