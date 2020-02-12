46a4a8a6494145abbf71d744e548560faafc7af1
Daransoto <901@holbertonschool.com>
2020-02-12 02:04:36 +0000

    Added utf8 encoding and fixed number of tweets printed

--- 0x14-javascript-web_scraping/102-search_twitter.js
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const base64 = require('base-64');
 const request = require('request');
-const headers = { Authorization: 'Basic ' + base64.encode(process.argv[2] + ':' + process.argv[3]), 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' };
+const utf8 = require('utf8');
+const headers = { Authorization: 'Basic ' + base64.encode(utf8.encode(process.argv[2] + ':' + process.argv[3])), 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' };
 const data = { grant_type: 'client_credentials' };
 request.post({ url: 'https://api.twitter.com/oauth2/token', headers: headers, qs: data }, function (err, res, body) {
   if (err) console.log(err);
@@ -12,7 +13,8 @@ request.post({ url: 'https://api.twitter.com/oauth2/token', headers: headers, qs
     request.get({ url: 'https://api.twitter.com/1.1/search/tweets.json', headers: headers2, qs: data2 }, function (err2, res2, body2) {
       if (err2) console.log(err2);
       else {
-        for (const stat of JSON.parse(body2).statuses) for (let i = 1; i < 6; i++) console.log(`[${stat.id}] ${stat.text} by ${stat.user.name}`);
+        let i = 0;
+        for (const stat of JSON.parse(body2).statuses) if (i++ < 5) console.log(`[${stat.id}] ${stat.text} by ${stat.user.name}`);
       }
     });
   }
